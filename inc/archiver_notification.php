55a385f000a7993e461fe3a940eefc0c119f1491
Rainer Müller <rainer@websimple.be>
2014-01-11 12:22:00 +0100

    laonction qui gère tout

---
+++ inc/archiver_notification.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Fonctions utiles au plugin Archive notifications
+ *
+ * @plugin     Archive notifications
+ * @copyright  2014
+ * @author     Rainer
+ * @licence    GNU/GPL
+ * @package    SPIP\Notifications_archive\Inc
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function inc_archiver_notification_dist($email, $subject, $o){
+    set_request('email',$email);
+    set_request('sujet',$subject); 
+    foreach($o AS $champ=>$valeur){
+        set_request($champ,$valeur);
+    }
+
+    formulaires_editer_objet_traiter('notification','new');
+
+
+}
+
+?>
